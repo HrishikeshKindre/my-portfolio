dcbfeb5d8fac9ac10ff3ce04509326bd9e3f98f6
Anonnee <hrishikindrej@gmail.com>
2025-06-19 21:02:23 -0400

    Initial project setup with Tailwind and CSS utilities

---
+++ my-portfolio/src/pags/Home.jsx
@@ -0,0 +1,21 @@
+
+export const Home = () => {
+    return <div className="min-h-screen">
+        {
+            /* Render a theme toggle */
+
+        }
+
+        {
+            /* Background effects */
+        }
+
+        {
+            /* Navbar */
+        }
+
+        {/*Main content area*/}
+
+        {/* Footer */}
+    </div>;
+}
